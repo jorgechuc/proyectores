78901ff60690b0cb8ba1ec216164fccd74bc9440
Jorge Luis Chuc Lopez <jorge.cl@campeche.tecnm.mx>
2026-04-15 08:33:45 -0600

    Se modificó el título del archivo README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+\## Proyectos ##
+Proyectos es una aplicación web de ASP.NET que gestionará las
+reservaciones y préstamos de proyectores a los docentes.
+
---
+++ WebApp/Controllers/ProyectoresController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ProyectoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return Content("<h1>Proyectores</h1>"
+            //    + "<p>Aquí se mostrar&aacute; la lista de proyectores registrados.</p>",
+            //    "text/html");
+            return new ContentResult()
+            {
+                Content = "<h1>Proyectores</h1>"
+                            + "<p>Aquí se mostrar&aacute; la lista de proyectores registrados.</p>",
+                ContentType = "text/html"
+            };
+        }
+
+        public IActionResult Details(int id)
+        {
+            return Content(
+                $"<h1>Nuevo proyector</h1>"
+                + $"<p>Aqu&iacute; se mostrar&aacute; la informaci&oacute;n del proyector {id}</p>",
+                "text/html");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return Content(
+                $"<h1>Dar de baja</h1>"
+                + $"<p>Aqu&iacute; se dar&aacute; de baja el proyector {id}</p>",
+                "text/html"
+                );
+        }
+    }
+}
