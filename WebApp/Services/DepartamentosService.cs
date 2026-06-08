abe0266237fa58decb6ef0058c6dcb8e64f3586d
Jorge Luis Chuc Lopez <jorge.cl@campeche.tecnm.mx>
2026-06-08 15:36:53 -0600

    Agrega el servicio de gestión de departamentos

---
+++ WebApp/Services/DepartamentosService.cs
@@ -0,0 +1,20 @@
+﻿using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Services
+{
+    public class DepartamentosService : IDepartamentosService
+    {
+        private readonly AppDbContext _contexto;
+
+        public DepartamentosService(AppDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public IEnumerable<Departamento> Get()
+        {
+            return _contexto.Departamentos.ToList<Departamento>();
+        }
+    }
+}
---
+++ WebApp/Services/IDepartamentosService.cs
@@ -0,0 +1,13 @@
+﻿using WebApp.Models;
+
+namespace WebApp.Services
+{
+    public interface IDepartamentosService
+    {
+        IEnumerable<Departamento> Get();
+        //Departamento? GetDepartamentoById(int id);
+        //void Add(Departamento departamento);
+        //Departamento Update(Departamento departamento);
+        //Departamento Delete(int id);
+    }
+}
